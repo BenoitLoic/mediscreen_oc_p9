08f4baae4bfe3ed88f7f11e7f3f4818818572a26
loic <shin.loicb@gmail.com>
2022-06-13 12:20:34 +0200

    Added Spring boot Skeleton.

---
+++ patientinfo/src/main/java/com/mediscreen/patientinfo/PatientinfoApplication.java
@@ -0,0 +1,13 @@
+package com.mediscreen.patientinfo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatientinfoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PatientinfoApplication.class, args);
+	}
+
+}
---
+++ patientinfo/src/test/java/com/mediscreen/patientinfo/PatientinfoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mediscreen.patientinfo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatientinfoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
