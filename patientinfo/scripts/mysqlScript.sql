2706cb5bfc1f5e9a0a3100e9bbe56745e604afd1
loic <shin.loicb@gmail.com>
2022-06-14 16:14:34 +0200

    Added homeController

--- patientinfo/scripts/mysqlScript.sql
+++ patientinfo/scripts/mysqlScript.sql
@@ -1,5 +1,7 @@
 CREATE DATABASE `MediscreenSql` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci;
 
+### Create Table patient
+
 CREATE TABLE `patient`
 (
     `id`            int          NOT NULL AUTO_INCREMENT,
@@ -16,21 +18,22 @@ CREATE TABLE `patient`
   DEFAULT CHARSET = utf8mb4
   COLLATE = utf8mb4_0900_ai_ci;
 
+# Add Patient
 INSERT INTO `MediscreenSql`.`patient` (`family_name`, `given_name`, `date_of_birth`, `sex`, `address`, `phone`)
 VALUES ('famillyNameTest3', 'givenNameTest3', '1949-01-01', 'F', '949 mysql address test', '222-303-4444');
 
-
-SELECT `patient`.`id`,
-       `patient`.`family_name`,
-       `patient`.`given_name`,
-       `patient`.`date_of_birth`,
-       `patient`.`sex`,
-       `patient`.`address`,
-       `patient`.`phone`
-FROM `MediscreenSql`.`patient`;
-
+# Read patient with familyNameX and givenNameX
 SELECT *
 FROM `MediscreenSql`.`patient`
 where patient.family_name = 'familyNameX' and patient.given_name='givenNameX';
 
-
+# Update Script
+UPDATE `MediscreenSql`.`patient`
+SET
+    `family_name` = '<{family_name: }>',
+    `given_name` = '<{given_name: }>',
+    `date_of_birth` = '<{date_of_birth: }>',
+    `sex` = '<{sex: }>',
+    `address` = '<{address: }>',
+    `phone` = '<{phone: }>'
+WHERE `id` = '<{expr}>';
---
+++ patientinfo/src/main/java/com/mediscreen/patientinfo/controller/impl/HomeController.java
@@ -0,0 +1,18 @@
+package com.mediscreen.patientinfo.controller.impl;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/**
+ * Controller for HomePage
+ * Contain a method that return the homepage
+ */
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String homePage(){
+        return "home";
+    }
+
+}
