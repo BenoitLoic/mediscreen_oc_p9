710ed7fa283fa93dc9c836b60d0200fe8d411ff4
loic <shin.loicb@gmail.com>
2022-06-18 18:23:19 +0200

    Added containerization for patientInfo application

--- patientinfo/scripts/mysqlScript.sql
+++ patientinfo/scripts/mysqlScript.sql
@@ -1,4 +1,4 @@
-CREATE DATABASE `MediscreenSql` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci;
+CREATE DATABASE `mediscreen_mysql_db` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci;
 
 ### Create Table patient
 
@@ -14,7 +14,7 @@ CREATE TABLE `patient`
     PRIMARY KEY (`id`),
     UNIQUE KEY `id_UNIQUE` (`id`)
 ) ENGINE = InnoDB
-  AUTO_INCREMENT = 6
+  AUTO_INCREMENT = 1
   DEFAULT CHARSET = utf8mb4
   COLLATE = utf8mb4_0900_ai_ci;
 
---
+++ patientinfo/scripts/sqlCreate.sql
@@ -0,0 +1,20 @@
+
+
+CREATE DATABASE IF NOT EXISTS `mediscreen_mysql_db` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci;
+
+### Create Table patient
+CREATE TABLE `patient`
+(
+    `id`            int          NOT NULL AUTO_INCREMENT,
+    `family_name`   varchar(45)  NOT NULL,
+    `given_name`    varchar(45)  NOT NULL,
+    `date_of_birth` date         NOT NULL,
+    `sex`           char(1)      NOT NULL,
+    `address`       varchar(100) NOT NULL,
+    `phone`         varchar(45)  NOT NULL,
+    PRIMARY KEY (`id`),
+    UNIQUE KEY `id_UNIQUE` (`id`)
+) ENGINE = InnoDB
+  AUTO_INCREMENT = 1
+  DEFAULT CHARSET = utf8mb4
+  COLLATE = utf8mb4_0900_ai_ci;
