fc7ae18bf44aecfc7fad174531c329b4cdaf8466
loic <shin.loicb@gmail.com>
2022-06-28 13:04:55 +0200

    added module for Assess app

---
+++ patientassessment/src/main/java/com/mediscreen/patientassessment/PatientassessmentApplication.java
@@ -0,0 +1,13 @@
+package com.mediscreen.patientassessment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatientassessmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PatientassessmentApplication.class, args);
+	}
+
+}
---
+++ patientassessment/src/test/java/com/mediscreen/patientassessment/PatientassessmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mediscreen.patientassessment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatientassessmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
